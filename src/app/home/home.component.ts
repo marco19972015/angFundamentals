1005e919ef1a3587ee728061c524035e17ffaa55
Marco Torres <marcoantonio.torres@icloud.com>
2023-10-16 20:09:18 -0400

    Create out first angular component, defined the application prefic for components, used inline templates and styles, accessing and displaying images using our assets and images folder, created additional components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'bot-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  ngOnInit(): void {
+    
+  }
 
 }
