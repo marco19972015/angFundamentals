4c16c4f14991159ecacd7ec803cbbc40917c98b5
Marco Torres <marcoantonio.torres@icloud.com>
2023-10-18 17:04:48 -0400

    Used the Output decorator and EventEmitter to pass data from the child component to the parent componet. We also we able to reduce the code on the parent component template

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IProduct } from '../catalog/product.model';
 
 @Component({
@@ -10,13 +10,15 @@ export class ProductDetailsComponent {
   
   @Input() product!: IProduct;
 
+  @Output() buy = new EventEmitter;
+
   getImageUrl(product: IProduct) {
     if (!product) return '';
     return '/assets/images/robot-parts/' + product.imageName
   }
 
-  addToCart(product: IProduct) {
-
+  buyButtonClicked(product: IProduct) {
+    this.buy.emit();
   }
 
 }
